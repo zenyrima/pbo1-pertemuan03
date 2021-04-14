23187ad58eaf31125cfe9097341cebceeb7ab784
zenyrima <rimadamayanti69@gmail.com>
2021-04-14 23:16:27 +0800

    Operator Binary Assigment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris5-7 adalah baris comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Zeny Rima Damayanti";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif= 3.89;
+}
