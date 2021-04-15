9fb407c6ea74f801bb0b69125654b4ca6450269a
zenyrima <rimadamayanti69@gmail.com>
2021-04-15 11:47:08 +0800

    Condition False

--- src/Percabangan/IfElse.java
+++ src/Percabangan/IfElse.java
@@ -2,13 +2,16 @@ package Percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 53000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if (uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+ angsul);
         }else {
-            System.out.println("Uang cukup, selamat menikamti");
+            System.out.println("Uang pas..Uwuw..");
         }
     }
 }
