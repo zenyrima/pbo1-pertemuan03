13e40cfa41773b56d4b86f34119a02d8283891c9
zenyrima <rimadamayanti69@gmail.com>
2021-04-15 11:57:40 +0800

    Switch

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,24 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case  2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("budil dit");
+                break;
+            default:
+                System.out.println("makhluk halus");
+                break;
+        }
+    }
+}
