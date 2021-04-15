254ff57c0590a0d041c37408c690fc0d6e27f745
zenyrima <rimadamayanti69@gmail.com>
2021-04-15 11:41:59 +0800

    Condition True

---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,15 @@
+package Percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if (uangDiDompet<totalBelanja){
+            System.out.println("Uang kurang, kurangi jajan anda");
+        }else {
+            System.out.println("Uang cukup, selamat menikamti");
+        }
+    }
+}
+
